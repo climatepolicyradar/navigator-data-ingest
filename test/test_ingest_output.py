d0eb5446da6b66aeafaf28cb8c1bc99a4a81597f
Mark <mark@climatepolicyradar.org>
2022-12-12 16:39:14 +0000

    Trigger actions commit.

--- test/test_ingest_output.py
+++ test/test_ingest_output.py
@@ -1,4 +1,14 @@
+from cloudpathlib import S3Path
+import json
 
+bucket_path = S3Path("s3://ingest-unit-test-pipeline-bucket/ingest_unit_test_parser_input/")
 
-def dummy_test():
+json_files = bucket_path.glob("*.json")
+
+json_data = []
+for json_file in json_files:
+    data = json.loads(json_file.read_text())
+    json_data.append(data)
+
+def test_dummy():
     assert True
