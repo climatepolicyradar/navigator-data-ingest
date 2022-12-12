c7048cbaf03415fc2745fbffce9393fe978016d4
Mark <mark@climatepolicyradar.org>
2022-12-12 17:27:41 +0000

    Updating file path.

--- test/test_ingest_output.py
+++ test/test_ingest_output.py
@@ -6,7 +6,7 @@ from test.s3_utils import create_dictionary_from_s3_bucket, read_local_s3_json_f
 output_data = create_dictionary_from_s3_bucket(
     bucket_path=S3Path("s3://ingest-unit-test-pipeline-bucket/ingest_unit_test_parser_input/"),
     name_key="document_id")
-expected_output_data = read_local_s3_json_file(file_path="data/docs_test_subset_parser_input_expected.json")
+expected_output_data = read_local_s3_json_file(file_path="test/data/docs_test_subset_parser_input_expected.json")
 
 
 def test_parser_input():
