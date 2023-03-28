f207eab37795dd2ea20d51d04ce4167c950a559a
Mark <mark@climatepolicyradar.org>
2023-03-28 13:50:53 +0100

    Removing the unneeded imports.

--- integration_tests/test_ingest_stage.py
+++ integration_tests/test_ingest_stage.py
@@ -1,34 +1,4 @@
 import pytest
-from cloudpathlib import S3Path
-import os
-import re
-
-from integration_tests.s3_utils import (
-    create_dictionary_from_s3_bucket,
-    read_local_s3_json_file,
-)
-
-ARCHIVE_FILE_PATTERN = re.compile(
-    r"^[0-9]+\-[0-9]+\-[0-9]+\-[0-9]+\-[0-9]+\-[0-9]+.json"
-)
-INGEST_PIPELINE_BUCKET = os.environ.get("INGEST_PIPELINE_BUCKET")
-DOCUMENT_NAME_KEY = os.environ.get("DOCUMENT_NAME_KEY")
-INGEST_OUTPUT_PREFIX = os.environ.get("INGEST_OUTPUT_PREFIX")
-PARSER_INPUT_EXPECTED_DATA_FILE_PATH = os.environ.get(
-    "PARSER_INPUT_EXPECTED_DATA_FILE_PATH"
-)
-TEST_DATA_FILE_PATH = os.environ.get("TEST_DATA_FILE_PATH")
-EXISTING_DOCUMENT_NAME = list(
-    read_local_s3_json_file(TEST_DATA_FILE_PATH)["updated_documents"].keys()
-)[0]
-parser_input_data = create_dictionary_from_s3_bucket(
-    bucket_path=S3Path(f"s3://{INGEST_PIPELINE_BUCKET}/{INGEST_OUTPUT_PREFIX}/"),
-    name_key=DOCUMENT_NAME_KEY,
-    glob_pattern="*.json",
-)
-expected_parser_input_data = read_local_s3_json_file(
-    file_path=PARSER_INPUT_EXPECTED_DATA_FILE_PATH
-)
 
 
 @pytest.mark.integration
