fda71a955c1a573d2696a5b5af04601e8bffd2d5
NextGenEng <58440325+THOR300@users.noreply.github.com>
2023-04-11 16:32:36 +0100

    Updating the integration tests (#34)
    Remove buckets before tests start incase there is left over data from
    another test.
    
    ---------
    
    Co-authored-by: Mark <mark@climatepolicyradar.org>

--- integration_tests/remove_test_buckets.py
+++ integration_tests/remove_test_buckets.py
@@ -7,15 +7,16 @@ from integration_tests.s3_utils import remove_bucket, remove_objects
 
 def remove(document_bucket_name: str, pipeline_bucket_name: str, region: str) -> None:
     """Remove the AWS infrastructure used in the unit integration_tests."""
-    s3_conn = boto3.resource("s3", region_name=region)
+    s3_resource = boto3.resource("s3", region_name=region)
+    s3_client = boto3.client("s3", region_name=region)
 
-    remove_objects(s3=s3_conn, bucket_name=document_bucket_name)
-    remove_objects(s3=s3_conn, bucket_name=pipeline_bucket_name)
+    if s3_resource.Bucket(document_bucket_name).creation_date:
+        remove_objects(s3=s3_resource, bucket_name=document_bucket_name)
+        remove_bucket(s3=s3_client, bucket_name=document_bucket_name)
 
-    s3_conn = boto3.client("s3", region_name=region)
-
-    remove_bucket(s3=s3_conn, bucket_name=document_bucket_name)
-    remove_bucket(s3=s3_conn, bucket_name=pipeline_bucket_name)
+    if s3_resource.Bucket(pipeline_bucket_name).creation_date:
+        remove_objects(s3=s3_resource, bucket_name=pipeline_bucket_name)
+        remove_bucket(s3=s3_client, bucket_name=pipeline_bucket_name)
 
 
 if __name__ == "__main__":
