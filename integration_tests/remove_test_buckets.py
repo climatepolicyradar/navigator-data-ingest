ddb914c4a5efe46b79fd5c5339a1a1a4ea16fb3a
Mark <mark@climatepolicyradar.org>
2023-04-11 14:42:01 +0100

    Updating bucket removal to catch non-existent bucket error.

--- integration_tests/remove_test_buckets.py
+++ integration_tests/remove_test_buckets.py
@@ -1,21 +1,27 @@
 import sys
 
 import boto3
+from botocore.client import ClientError
 
 from integration_tests.s3_utils import remove_bucket, remove_objects
 
 
 def remove(document_bucket_name: str, pipeline_bucket_name: str, region: str) -> None:
     """Remove the AWS infrastructure used in the unit integration_tests."""
-    s3_conn = boto3.resource("s3", region_name=region)
+    s3_resource = boto3.resource("s3", region_name=region)
+    s3_client = boto3.client("s3", region_name=region)
 
-    remove_objects(s3=s3_conn, bucket_name=document_bucket_name)
-    remove_objects(s3=s3_conn, bucket_name=pipeline_bucket_name)
+    try:
+        remove_objects(s3=s3_resource, bucket_name=document_bucket_name)
+        remove_bucket(s3=s3_client, bucket_name=document_bucket_name)
+    except ClientError:
+        pass  # Bucket does not exist
 
-    s3_conn = boto3.client("s3", region_name=region)
-
-    remove_bucket(s3=s3_conn, bucket_name=document_bucket_name)
-    remove_bucket(s3=s3_conn, bucket_name=pipeline_bucket_name)
+    try:
+        remove_objects(s3=s3_resource, bucket_name=pipeline_bucket_name)
+        remove_bucket(s3=s3_client, bucket_name=pipeline_bucket_name)
+    except ClientError:
+        pass  # Bucket does not exist
 
 
 if __name__ == "__main__":
