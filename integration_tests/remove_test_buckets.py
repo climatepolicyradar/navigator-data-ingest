0fffcc6476cd2f20962222a4d752104a32ec5a2f
Mark <mark@climatepolicyradar.org>
2023-04-11 14:53:39 +0100

    updating to only remove bucket if it exists.

--- integration_tests/remove_test_buckets.py
+++ integration_tests/remove_test_buckets.py
@@ -1,7 +1,6 @@
 import sys
 
 import boto3
-from botocore.client import ClientError
 
 from integration_tests.s3_utils import remove_bucket, remove_objects
 
@@ -11,17 +10,13 @@ def remove(document_bucket_name: str, pipeline_bucket_name: str, region: str) ->
     s3_resource = boto3.resource("s3", region_name=region)
     s3_client = boto3.client("s3", region_name=region)
 
-    try:
+    if s3_resource.Bucket(document_bucket_name).creation_date:
         remove_objects(s3=s3_resource, bucket_name=document_bucket_name)
         remove_bucket(s3=s3_client, bucket_name=document_bucket_name)
-    except ClientError:
-        pass  # Bucket does not exist
 
-    try:
+    if s3_resource.Bucket(pipeline_bucket_name).creation_date:
         remove_objects(s3=s3_resource, bucket_name=pipeline_bucket_name)
         remove_bucket(s3=s3_client, bucket_name=pipeline_bucket_name)
-    except ClientError:
-        pass  # Bucket does not exist
 
 
 if __name__ == "__main__":
